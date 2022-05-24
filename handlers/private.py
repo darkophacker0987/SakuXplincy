6c3b855394fd2c447ceaf507b30a48fc1bcc27d8
darkophacker0987 <82193878+darkophacker0987@users.noreply.github.com>
2022-05-24 10:19:41 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -35,22 +35,17 @@ async def start_(client: Client, message: Message):
        ),
     )
 
-@Client.on_message(command(["help"]) & filters.group & ~filters.edited)
-async def helper(ok, message: Message):
-    await message.reply_text(
-        f"""💞 Hello! Following are the commands available for **{bn}** - __A Group Voice Chat Music Player__.
-The commands I currently support are:
-
-🔥 **Users Commands :**
-⚜️ /play - **[ Groups Only ]** > __Plays the replied audio file or YouTube video through link.__
-⚜️ /song - **[ Groups & DM ]** > __Uploads the searched song in the chat.__
-
-
-
-🔰 **Admin & Sudo Users Commands :**
-⚜️ /pause - **[Groups Only ]** > __Pause Voice Chat Music.__
-⚜️ /resume - **[Groups Only ]** > __Resume Voice Chat Music.__
-⚜️ /skip - **[Groups Only ]** > __Skips the current Music Playing In Voice Chat.__
-⚜️ /stop - **[Groups Only ]** > __Clears The Queue as well as ends Voice Chat Music.__""")
-
-        
+@Client.on_message(command(["repo"]) & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/c247284e8ddc50f01a314.png",
+        caption=f"""""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "💥 Repo 💞", url=f"https://github.com/teamUltronX")
+                ]
+            ]
+        ),
+    )
